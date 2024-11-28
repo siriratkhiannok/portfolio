88fb8116afc7e25880e638ed1b03e88d16b4abff
Kaseamsan Keawjeen <66022916@up.ac.th>
2024-11-28 16:23:50 +0700

    navBar Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 import anime from "../public/img/anime.svg";
 import ball1 from "../public/img/ball1.svg";
 import ball2 from "../public/img/ball2.svg";
+import ball3 from "../public/img/ball3.svg";
 
 export default function Home() {
   return (
     <div
-      className="bg-black w-auto box-border"
+      className="bg-black w-auto box-border "
       style={{ width: "100dvw", height: "100dvh" }}
     >
-      <div className="absolute w-full">
-
-      <div className="text-white font-semibold flex justify-end mr-28 h-36  bg-transparent">
-        <p className="ml-6  mr-6  pt-10 pl-3 pr-3 pb-7 h-24 border border-red-500 bg-gradient-to-b from-slate-800 to-black rounded-b-full" >Home</p>
-        <p className="ml-6  mr-6  pt-10 pl-3 pr-3 pb-7 h-24 border border-red-500 bg-gradient-to-b from-slate-800 to-black rounded-b-full" >My Skills</p>
-        <p className="ml-6  mr-6  pt-10 pl-3 pr-3 pb-7 h-28 border border-red-500 bg-gradient-to-b from-slate-800 to-black rounded-b-full" >My Projects</p>
-        <p className="ml-10 mr-6  pt-10 pl-3 pr-3 pb-7 h-28 border border-red-500 bg-gradient-to-b from-slate-800 to-black rounded-b-full" > My Education</p>
-      </div>
+      <title>Portfolio</title>
+      <div className="absolute w-full z-10">
+        <div className=" text-white font-semibold flex justify-end mr-96 h-36 bg-transparent">
+          <p className="chouce_Default">Home</p>
+          <p className="choice1 ">My Skills</p>
+          <p className="choice2">My Projects</p>
+        </div>
       </div>
       <img width={360} src={ball1.src} className="absolute" alt="" />
+      <img width={300} src={ball3.src} className="absolute top-0 right-0 " alt="" />
       <img
         width={400}
         src={ball2.src}
